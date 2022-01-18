983dcf070f0aed0d130743c5d242168f5685d5ac
joaopaulopires <joaopaulopiresdev@gmail.com>
2022-01-18 07:59:22 -0300

    Fix debug wsgi

--- gestao_vendas/wsgi.py
+++ gestao_vendas/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from static import Cling
+from dj_static import Cling
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_vendas.settings')
 
 application = Cling(get_wsgi_application())
